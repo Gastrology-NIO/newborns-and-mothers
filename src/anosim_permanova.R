294a67e7e48f082a45ea7aab83b3cdd70a1be959
Joanna <jm.ziemska@gmail.com>
2026-03-12 22:32:19 +0100

    Create anosim_permanova.R

---
+++ src/anosim_permanova.R
@@ -0,0 +1,22 @@
+
+sample_data(ps_genus)$location_type<-paste(sample_data(ps_genus)$Location, sample_data(ps_genus)$Type, sample_data(ps_genus)$Stadium)
+locations_all<-unique(sample_data(ps_genus)$location_type)
+res_anosim<-data.frame(location=c("a"),location2=c("a"),statistic=c("a"), p_val=c("a"))
+for (location in locations_all){
+for (location2 in locations_all){
+if (location!=location2){
+
+  ps_tmp<-subset_samples(ps_genus, location_type %in% c(location, location2))
+  ps_tmp <- microbiome::transform(ps_tmp, "clr")
+  metadata_sub <- data.frame(sample_data(ps_tmp))
+  
+  anosim_test<-anosim(phyloseq::distance(ps_tmp, method="euclidean"), 
+                             metadata_sub$location_type, permutations = 9999)
+  res_anosim[nrow(res_anosim) + 1,]<-c(location,location2, anosim_test$statistic, anosim_test$signif)
+
+}}}
+res_anosim<-res_anosim[c(2:nrow(res_anosim)),]
+write.csv2(res_anosim, paste0(folder_out, "anosim_euclidean_dist_bh.csv"))
+
+
+
