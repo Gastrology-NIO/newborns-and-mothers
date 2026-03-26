e0fdeddd5bf599501a906ce8ed4da5a515b01828
Joanna <jm.ziemska@gmail.com>
2026-03-26 16:52:54 +0100

    Update PCoA.R

--- src/PCoA.R
+++ src/PCoA.R
@@ -60,20 +60,56 @@ PCoA <- function(ps_all, folder_out){
 PCoA_selected<- function(ps_all, save_path, type){
   pairs<- data.frame(loc1=c('Stool;Mother', 'Cervix;Mother', 'Cheek;Mother', 'Mother placenta;Mother'), loc2=c('Rectum;Newborn','Placenta;Newborn','Stomach;Newborn','Placenta;Newborn'))
   ps_genus<-tax_glom(ps_all, "genus")
+  
+  sample_data(ps_genus)$location_type<-paste0(sample_data(ps_genus)$Location, ";", sample_data(ps_genus)$Stadium,";", sample_data(ps_genus)$Type)
+  physeq_clr <- microbiome::transform(ps_genus, "clr")
+  ps_dist_matrix <- phyloseq::distance(physeq_clr, method ="euclidean")
+  control_adonis<-pairwise.adonis(ps_dist_matrix, phyloseq::sample_data(ps_genus)$location_type, p.adjust.m="BH")
+  
   ps_genus_type<-subset_samples(ps_genus, Type %in% c("K"))
-  type<-"K"
+  type<-"B"
 plots <- list()
   for (pair_no in 1:nrow(pairs)) {
 
    sample_data(ps_genus_type)$mergedLocStad<-mapply(paste0, sample_data(ps_genus_type)$Location,";", sample_data(ps_genus_type)$Stadium) 
     ps_tmp<-subset_samples(ps_genus_type, mergedLocStad %in% pairs[pair_no,])
     pseq.compositional <- microbiome::transform(ps_tmp, "clr")
+    ad_res <- control_adonis[control_adonis$pair  %in% c(paste(paste0(pairs$loc1[pair_no], ";",type),"vs" ,paste0(pairs$loc2[pair_no], ";",type)), paste(paste0(pairs$loc2[pair_no], ";",type),"vs" ,paste0(pairs$loc1[pair_no], ";",type))), ]
+    ad_label <- paste0(
+        "R² = ", round(ad_res$R2, 3), "\n",
+        "p = ", signif(ad_res$p.adjusted, 3)
+    )
     ps.pcoa.a <- ordinate(pseq.compositional, method="PCoA", distance="euclidean")
     p <- plot_ordination(pseq.compositional, ps.pcoa.a, color = "mergedLocStad", label="Organism_id") + 
     geom_point(size = 3)+
   stat_ellipse(type = "norm")+
   scale_color_manual(values = c("#e73785ff", "#2db62bff"))
 
+    if ((paste0(pairs$loc1[pair_no], ";",type) == "Cheek;Mother;K") & (paste0(pairs$loc1[pair_no], ";",type) == "Stomach;Newborn;K")){
+        p <- p +
+  annotation_custom(
+    grob = textGrob(
+      ad_label,
+      x = unit(0.98, "npc"),   # prawy margines
+      y = unit(0.98, "npc"),   # górny margines
+      hjust = 1, vjust = 1,    # wyrównanie do prawej i góry
+      gp = gpar(col = "black", fontsize = 10)
+    )
+  ) +
+  coord_cartesian(clip = "off")
+    } else {
+          p<-p+annotation_custom(
+    grob = textGrob(
+      ad_label,
+      x = unit(0.02, "npc"),   # lewy margines
+      y = unit(0.98, "npc"),   # górny margines
+      hjust = 0, vjust = 1,
+      gp = gpar(col = "black", fontsize = 10)
+    )
+  ) +
+  coord_cartesian(clip = "off")
+
+    }
 
     
    plots[[pair_no]] <- p 
