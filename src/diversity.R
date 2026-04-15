1da446d08d1920e81a17e419ae6d6607d8fedeb8
Joanna <jm.ziemska@gmail.com>
2026-04-15 11:42:24 +0200

    Update diversity.R

--- src/diversity.R
+++ src/diversity.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggsignif)
 library(patchwork)
+library(ggpubr)
 
 
 
@@ -116,30 +117,34 @@ plot_richness_with_p_val<-function(ps_genus, measure){
     return(plot)
 }
 
-# e737e3ff + 2db62bff
-# e73785ff
-plots <- list()
+add_comparision<-function(comparision, p, tmp){
+  p<-p+  stat_compare_means(
+    comparisons = comparisons,
+    method = "wilcox.test",
+    label = "p.signif",
+    data = tmp
+  ) 
+  return(p)
+}
 
-# było f8766dff 00bfc4ff
-shannon<-plot_richness_with_p_val(ps_genus, "Shannon")
-shannon
-plots[[1]] <- shannon 
 
-chao1<-plot_richness_with_p_val(ps_genus, "Chao1")
-chao1
-plots[[2]] <- chao1
+plot_diversity_in_batch <- function(ps, folder_out){
+  plots <- list()
 
-simpson<-plot_richness_with_p_val(ps_genus, "Simpson")
-simpson
-plots[[3]] <- simpson 
-
-observed<-plot_richness_with_p_val(ps_genus, "Observed")
-observed
-plots[[4]] <- observed 
-
-fisher<-plot_richness_with_p_val(ps_genus, "Fisher")
-fisher
-plots[[5]] <- fisher 
+  shannon<-plot_richness_with_p_val(ps, "Shannon")
+  plots[[1]] <- shannon 
+  
+  chao1<-plot_richness_with_p_val(ps, "Chao1")
+  plots[[2]] <- chao1
+  
+  simpson<-plot_richness_with_p_val(ps, "Simpson")
+  plots[[3]] <- simpson 
+  
+  observed<-plot_richness_with_p_val(ps, "Observed")
+  plots[[4]] <- observed 
+  
+  fisher<-plot_richness_with_p_val(ps, "Fisher")
+  plots[[5]] <- fisher 
 
 
   p<-wrap_plots(plots, ncol = 2) +
@@ -151,15 +156,6 @@ plots[[5]] <- fisher
     width = 12, height = 18,
     limitsize  = FALSE
   )
-
-add_comparision<-function(comparision, p, tmp){
-  p<-p+  stat_compare_means(
-    comparisons = comparisons,
-    method = "wilcox.test",
-    label = "p.signif",
-    data = tmp
-  ) 
-  return(p)
 }
 
 
@@ -201,237 +197,204 @@ add_clamr_long <- function(plot, x1, x2, y, label_star) {
 
 
 plot_richness_with_p_val_by_pairs<-function(ps_genus, measure, type){
-pairs<- c('Stool;Mother',  'Cheek;Mother', 'Mother placenta;Mother','Rectum;Neonate', 'Cervix;Mother','Placenta;Neonate','Stomach;Neonate','Placenta;Neonate')
-pairs_loc<- data.frame(loc1=c('Stool', 'Cervix', 'Cheek', 'Mother placenta'), loc2=c('Rectum','Placenta','Stomach','Placenta'))
-sample_data(ps_genus)$mergedLocStad<-mapply(paste0, sample_data(ps_genus)$Location,";", sample_data(ps_genus)$Stadium)
-df <- plot_richness(ps_genus, "Location", measures = measure)$data
-tmp<-df[df$mergedLocStad %in% pairs,]
-tmp<-tmp[tmp$Type == type,]
-order_box<-c('Stool', 'Rectum', 'Cheek','Stomach', 'Mother placenta', 'Placenta', 'Cervix')
-
-tmp$Location <- factor(tmp$Location,
-                       levels = order_box)
-
-p<-ggplot(tmp, aes(Location, value, fill=Type)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.2, , size = 1) +
+  pairs<- c('Stool;Mother',  'Cheek;Mother', 'Mother placenta;Mother','Rectum;Neonate', 'Cervix;Mother','Placenta;Neonate','Stomach;Neonate','Placenta;Neonate')
+  pairs_loc<- data.frame(loc1=c('Stool', 'Cervix', 'Cheek', 'Mother placenta'), loc2=c('Rectum','Placenta','Stomach','Placenta'))
+  sample_data(ps_genus)$mergedLocStad<-mapply(paste0, sample_data(ps_genus)$Location,";", sample_data(ps_genus)$Stadium)
+  df <- plot_richness(ps_genus, "Location", measures = measure)$data
+  tmp<-df[df$mergedLocStad %in% pairs,]
+  tmp<-tmp[tmp$Type == type,]
+  order_box<-c('Stool', 'Rectum', 'Cheek','Stomach', 'Mother placenta', 'Placenta', 'Cervix')
   
-  # globalny test Kruskala dla Location
-  stat_compare_means(
-    aes(group = 1),   # ważne: ignoruje Type
-    method = "kruskal.test",
-    label = "p.format"
-  ) +
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_fill_manual(values = c("LP" = "#e73785ff","TP" = "#2db62bff"))+ggtitle(measure) +
-      labs(x =NULL, y = NULL)+ theme(legend.position="none")
-  p
-for (pair_no in 1:nrow(pairs_loc)) {
-  x1<-0
-  x2<-0
-  for (i in 1:length(order_box)){
-    if (order_box[i]==pairs_loc$loc1[pair_no]){
-      x1<-i
+  tmp$Location <- factor(tmp$Location,
+                         levels = order_box)
+  
+  p<-ggplot(tmp, aes(Location, value, fill=Type)) +
+    geom_boxplot() +
+    geom_jitter(width = 0.2, , size = 1) +
+    
+    # globalny test Kruskala dla Location
+    stat_compare_means(
+      aes(group = 1),   # ważne: ignoruje Type
+      method = "kruskal.test",
+      label = "p.format"
+    ) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_fill_manual(values = c("LP" = "#e73785ff","TP" = "#2db62bff"))+ggtitle(measure) +
+        labs(x =NULL, y = NULL)+ theme(legend.position="none")
+    p
+  for (pair_no in 1:nrow(pairs_loc)) {
+    x1<-0
+    x2<-0
+    for (i in 1:length(order_box)){
+      if (order_box[i]==pairs_loc$loc1[pair_no]){
+        x1<-i
+      }
+          if (order_box[i]==pairs_loc$loc2[pair_no]){
+        x2<-i
+      }
     }
-        if (order_box[i]==pairs_loc$loc2[pair_no]){
-      x2<-i
+    if (x2<x1){
+      x1_tmp<-x2
+      x2_tmp<-x1
+      x1<-x1_tmp
+      x2<-x2_tmp
     }
-  }
-  if (x2<x1){
-    x1_tmp<-x2
-    x2_tmp<-x1
-    x1<-x1_tmp
-    x2<-x2_tmp
-  }
-  print(x1)
-  print(x2)
-  comparisons <- combn(c(pairs_loc$loc1[pair_no], pairs_loc$loc2[pair_no]), 2, simplify = FALSE)
-  p_value <- wilcox.test(value ~ Location, data = df %>% filter(Location %in% c(pairs_loc[pair_no,])))$p.value
-  p_value_star <- p_to_stars(p_value)
-  tmp2<-df[df$Location %in% pairs_loc[pair_no,],]
-  p<-add_clamr_long(  plot = p,
-      x1 = x1,      # pozycja A
-      x2 = x2,      # pozycja C
-      y = max(tmp2$value) * 1.1,
-      label_star = p_value_star)
-  }
-  return(p)
+    print(x1)
+    print(x2)
+    comparisons <- combn(c(pairs_loc$loc1[pair_no], pairs_loc$loc2[pair_no]), 2, simplify = FALSE)
+    p_value <- wilcox.test(value ~ Location, data = df %>% filter(Location %in% c(pairs_loc[pair_no,])))$p.value
+    p_value_star <- p_to_stars(p_value)
+    tmp2<-df[df$Location %in% pairs_loc[pair_no,],]
+    p<-add_clamr_long(  plot = p,
+        x1 = x1,      # pozycja A
+        x2 = x2,      # pozycja C
+        y = max(tmp2$value) * 1.1,
+        label_star = p_value_star)
+    }
+    return(p)
+}
+
+# type<-"LP"
+# type<-"TP"
+
+plot_with_batch1<-function(ps, folder_out, type){
+  plots <- list()
+  
+  shannon<-plot_richness_with_p_val_by_pairs(ps, "Shannon", type)
+  plots[[1]] <- shannon 
+  
+  chao1<-plot_richness_with_p_val_by_pairs(ps, "Chao1", type)
+  plots[[2]] <- chao1
+  
+  simpson<-plot_richness_with_p_val_by_pairs(ps, "Simpson", type)
+  plots[[3]] <- simpson 
+  
+  observed<-plot_richness_with_p_val_by_pairs(ps, "Observed", type)
+  plots[[4]] <- observed 
+  
+  fisher<-plot_richness_with_p_val_by_pairs(ps, "Fisher", type)
+  plots[[5]] <- fisher 
+  
+
+
+  p<-wrap_plots(plots, ncol = 2) +
+    plot_annotation(tag_levels = "A")
+  
+  ggsave(
+      paste0(folder_out, type,"_by_pairs_diversity.svg"),
+      plot = p,
+    width = 8, height = 12,
+    limitsize  = FALSE
+  )
+
 }
 
 
-plots <- list()
-type<-"LP"
 
-# było f8766dff 00bfc4ff
-shannon<-plot_richness_with_p_val_by_pairs(ps_genus, "Shannon", type)
-shannon
-plots[[1]] <- shannon 
-
-chao1<-plot_richness_with_p_val_by_pairs(ps_genus, "Chao1", type)
-chao1
-plots[[2]] <- chao1
-
-simpson<-plot_richness_with_p_val_by_pairs(ps_genus, "Simpson", type)
-simpson
-plots[[3]] <- simpson 
-
-observed<-plot_richness_with_p_val_by_pairs(ps_genus, "Observed", type)
-observed
-plots[[4]] <- observed 
-
-fisher<-plot_richness_with_p_val_by_pairs(ps_genus, "Fisher", type)
-fisher
-plots[[5]] <- fisher 
-
-
-
-  p<-wrap_plots(plots, ncol = 2) +
-    plot_annotation(tag_levels = "A")
+calc_wilcoxon_between_diversity<-function(ps){
+  richness<-estimate_richness(ps)
+  rownames(richness)<-sub("X", "", rownames(ps))
   
-  ggsave(
-      paste0(folder_out, type,"_by_pairs_diversity.svg"),
-      plot = p,
-    width = 8, height = 12,
-    limitsize  = FALSE
-  )
-
-
-plots <- list()
-type<-"TP"
-
-# było f8766dff 00bfc4ff
-shannon<-plot_richness_with_p_val_by_pairs(ps_genus, "Shannon", type)
-shannon
-plots[[1]] <- shannon 
-
-chao1<-plot_richness_with_p_val_by_pairs(ps_genus, "Chao1", type)
-chao1
-plots[[2]] <- chao1
-
-simpson<-plot_richness_with_p_val_by_pairs(ps_genus, "Simpson", type)
-simpson
-plots[[3]] <- simpson 
-
-observed<-plot_richness_with_p_val_by_pairs(ps_genus, "Observed", type)
-observed
-plots[[4]] <- observed 
-
-fisher<-plot_richness_with_p_val_by_pairs(ps_genus, "Fisher", type)
-fisher
-plots[[5]] <- fisher 
-
-
-
-  p<-wrap_plots(plots, ncol = 2) +
-    plot_annotation(tag_levels = "A")
+  sample_data(ps)$mergedLocStad<-mapply(paste0, sample_data(ps)$Location,";", sample_data(ps)$Stadium)
+  sample_data(ps)$location_type<-paste(sample_data(ps)$mergedLocStad, sample_data(ps)$Type)
+  tmp<-merge(richness, sample_data(ps), by=0)
+  locations_all<-unique(tmp$location_type)
+  locations_all
+  res_wil<-data.frame(location=c("a"),location2=c("a"),  chao1=c("a"), shannon=c("a"), observed=c("a"), fisher=c("a"), simpson=c("a"))
+  for (location in locations_all){
+  for (location2 in locations_all){
+  if (location!=location2){
   
-  ggsave(
-      paste0(folder_out, type,"_by_pairs_diversity.svg"),
-      plot = p,
-    width = 8, height = 12,
-    limitsize  = FALSE
-  )
-
-
-
-richness<-estimate_richness(ps_genus)
-rownames(richness)<-sub("X", "", rownames(richness))
-
-sample_data(ps_genus)$mergedLocStad<-mapply(paste0, sample_data(ps_genus)$Location,";", sample_data(ps_genus)$Stadium)
-sample_data(ps_genus)$location_type<-paste(sample_data(ps_genus)$mergedLocStad, sample_data(ps_genus)$Type)
-tmp<-merge(richness, sample_data(ps_genus), by=0)
-locations_all<-unique(tmp$location_type)
-locations_all
-res_wil<-data.frame(location=c("a"),location2=c("a"),  chao1=c("a"), shannon=c("a"), observed=c("a"), fisher=c("a"), simpson=c("a"))
-for (location in locations_all){
-for (location2 in locations_all){
-if (location!=location2){
-
-  location_tmp<- tmp[tmp$location_type %in% c(location,location2),]
-  res_chao<-pairwise.wilcox.test(location_tmp$Chao1, location_tmp$location_type, 
-                                 p.adjust.method = "BH")$p.value[1]
-  res_shan<-pairwise.wilcox.test(location_tmp$Shannon, location_tmp$location_type, 
-                                 p.adjust.method = "BH")$p.value[1]
-  # pairwise.wilcox.test(Shannon ~ Type, data = location_tmp)$p.value
-  res_simpson<-pairwise.wilcox.test(location_tmp$Simpson, location_tmp$location_type, 
-                                    p.adjust.method = "BH")$p.value[1]
-  # pairwise.wilcox.test(Simpson ~ Type, data = location_tmp)$p.value
-  res_fisher<-pairwise.wilcox.test(location_tmp$Fisher, location_tmp$location_type, 
+    location_tmp<- tmp[tmp$location_type %in% c(location,location2),]
+    res_chao<-pairwise.wilcox.test(location_tmp$Chao1, location_tmp$location_type, 
                                    p.adjust.method = "BH")$p.value[1]
-  # pairwise.wilcox.test(Fisher ~ Type, data = location_tmp)$p.value
-  res_observed<-pairwise.wilcox.test(location_tmp$Observed, location_tmp$location_type, 
+    res_shan<-pairwise.wilcox.test(location_tmp$Shannon, location_tmp$location_type, 
+                                   p.adjust.method = "BH")$p.value[1]
+    # pairwise.wilcox.test(Shannon ~ Type, data = location_tmp)$p.value
+    res_simpson<-pairwise.wilcox.test(location_tmp$Simpson, location_tmp$location_type, 
+                                      p.adjust.method = "BH")$p.value[1]
+    # pairwise.wilcox.test(Simpson ~ Type, data = location_tmp)$p.value
+    res_fisher<-pairwise.wilcox.test(location_tmp$Fisher, location_tmp$location_type, 
                                      p.adjust.method = "BH")$p.value[1]
-  # pairwise.wilcox.test(Observed ~ Type, data = location_tmp)$p.value
-  res_wil[nrow(res_wil) + 1,]<-c(location, location2, res_chao, res_shan, res_simpson, res_fisher, res_observed)
-}}}
-res_wil<-res_wil[c(2:nrow(res_wil)),]
-
+    # pairwise.wilcox.test(Fisher ~ Type, data = location_tmp)$p.value
+    res_observed<-pairwise.wilcox.test(location_tmp$Observed, location_tmp$location_type, 
+                                       p.adjust.method = "BH")$p.value[1]
+    # pairwise.wilcox.test(Observed ~ Type, data = location_tmp)$p.value
+    res_wil[nrow(res_wil) + 1,]<-c(location, location2, res_chao, res_shan, res_simpson, res_fisher, res_observed)
+  }}}
+  res_wil<-res_wil[c(2:nrow(res_wil)),]
+}
 ####
 
 
-library(ggpubr)
 
-df <- plot_richness(ps_genus, "Location", measures = "Fisher")$data
 
-ggplot(df, aes(Location, value, fill = Type)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.2, size = 1) +
-  facet_wrap(~Stadium, scales = "free_x") +
+plot_with_clams_m_new <- function(ps) {
+
+  ## between different locations
+  df <- plot_richness(ps_genus, "Location", measures = "Shannon")$data
   
-  # globalny test Kruskala dla Location
-  stat_compare_means(
-    aes(group = 1),
-    method = "kruskal.test",
-    label = "p.format"
-  ) +
+  # listy porównań osobno dla Mother i Newborn
+  comparisons_mother <- combn(unique(df$Location[df$Stadium == "Mother"]), 2, simplify = FALSE)
+  comparisons_newborn <- combn(unique(df$Location[df$Stadium == "Neonate"]), 2, simplify = FALSE)
   
-  # klamry: B vs K w obrębie każdej Location
-  stat_compare_means(
-    aes(group = Type),
-    method = "wilcox.test",
-    label = "p.format"   # <--- tu zmieniamy gwiazdki na p-value
-  ) +
   
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  p<-ggplot(df, aes(Location, value, fill = Type)) +
+    geom_boxplot() +
+    geom_jitter(width = 0.2) +
+    facet_wrap(~Stadium, scales = "free_x") +
+    
+    # globalny test Kruskala dla Location
+    stat_compare_means(
+      aes(group = 1),   # ważne: ignoruje Type
+      method = "kruskal.test",
+      label = "p.format"
+    ) +
+    
+    # klamry dla Mother
+    stat_compare_means(
+      comparisons = comparisons_mother,
+      method = "wilcox.test",
+      label = "p.signif",
+      data = subset(df, Stadium == "Mother")
+    ) +
+    
+    # klamry dla Newborn
+    stat_compare_means(
+      comparisons = comparisons_newborn,
+      method = "wilcox.test",
+      label = "p.signif",
+      data = subset(df, Stadium == "Neonate")
+    ) +
+    
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  return(p)
+
+}
 
 
 
-## between different locations
-df <- plot_richness(ps_genus, "Location", measures = "Shannon")$data
+# df <- plot_richness(ps_genus, "Location", measures = "Fisher")$data
 
-# listy porównań osobno dla Mother i Newborn
-comparisons_mother <- combn(unique(df$Location[df$Stadium == "Mother"]), 2, simplify = FALSE)
-comparisons_newborn <- combn(unique(df$Location[df$Stadium == "Neonate"]), 2, simplify = FALSE)
-
-
-ggplot(df, aes(Location, value, fill = Type)) +
-  geom_boxplot() +
-  geom_jitter(width = 0.2) +
-  facet_wrap(~Stadium, scales = "free_x") +
+# ggplot(df, aes(Location, value, fill = Type)) +
+#   geom_boxplot() +
+#   geom_jitter(width = 0.2, size = 1) +
+#   facet_wrap(~Stadium, scales = "free_x") +
   
-  # globalny test Kruskala dla Location
-  stat_compare_means(
-    aes(group = 1),   # ważne: ignoruje Type
-    method = "kruskal.test",
-    label = "p.format"
-  ) +
+#   # globalny test Kruskala dla Location
+#   stat_compare_means(
+#     aes(group = 1),
+#     method = "kruskal.test",
+#     label = "p.format"
+#   ) +
   
-  # klamry dla Mother
-  stat_compare_means(
-    comparisons = comparisons_mother,
-    method = "wilcox.test",
-    label = "p.signif",
-    data = subset(df, Stadium == "Mother")
-  ) +
+#   # klamry: B vs K w obrębie każdej Location
+#   stat_compare_means(
+#     aes(group = Type),
+#     method = "wilcox.test",
+#     label = "p.format"   # <--- tu zmieniamy gwiazdki na p-value
+#   ) +
   
-  # klamry dla Newborn
-  stat_compare_means(
-    comparisons = comparisons_newborn,
-    method = "wilcox.test",
-    label = "p.signif",
-    data = subset(df, Stadium == "Neonate")
-  ) +
-  
-  theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#   theme_bw() +
+#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
